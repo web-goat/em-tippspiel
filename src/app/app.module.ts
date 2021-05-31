085237a5390ac7122ca3d0bced232cc266259789
Vincent Uhlich <vinciii97@gmail.com>
2021-05-31 20:39:06 +0200

    New Main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBAjxL9tCwtqwkoBa9w_bCS-VdVBFo7mLY",
+    authDomain: "em-tippsp.firebaseapp.com",
+    databaseURL: "https://em-tippsp-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "em-tippsp",
+    storageBucket: "em-tippsp.appspot.com",
+    messagingSenderId: "276511225832",
+    appId: "1:276511225832:web:20704b0ebf270d06c6565d",
+    measurementId: "G-3T4QKZPVQF"
+  }
 };
 
 /*
